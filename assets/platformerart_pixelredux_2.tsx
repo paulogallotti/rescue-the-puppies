58a93a8f9181aa5ee7cbeb82b1bd43d8dd4e44fe
Paulo Gallotti Rodrigues <paulogallotti@gmail.com>
2023-02-22 19:58:41 -0800

    added assets and fixed venv

---
+++ assets/platformerart_pixelredux_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="test1" tilewidth="21" tileheight="21" tilecount="900" columns="30">
+ <image source="platformerart_pixelredux_2/Tilemap/tilemap_packed.png" width="630" height="630"/>
+</tileset>
