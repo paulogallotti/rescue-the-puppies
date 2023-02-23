e022ebbcd0f2d63220279486bfd9cea7f94aad8e
Paulo Gallotti Rodrigues <paulogallotti@gmail.com>
2023-02-22 22:11:12 -0800

    first playable version of the game

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# rescue-the-puppies
-Mariana
+# Rescue the Puppies
+
+## Introduction
+
+Based on https://api.arcade.academy/en/latest/examples/template_platformer.html#template-platformer
+
+## Getting Started
+
+1. Create virtual environment
+
+```python3 -m venv venv (.gitignore will ignore any folder name that ends with "venv")```
+
+2. Activate the virtual environment
+
+```source venv/bin/activate```
+
+3. Install dependencies
+
+```pip install -r requirements.txt```
+
+4. If you need to deactivate the venv
+
+```deactivate```
+
+## Creator
+
+Mariana Vommaro Rodrigues
--- rescue-the-puppies/main.py
+++ rescue-the-puppies/main.py
@@ -2,77 +2,208 @@
 Rescue the Puppies
 
 All game artwork from www.kenney.nl
-Game sounds and tile maps by author
+Tile maps by author
 """
 
 import arcade
-import pathlib
+#import pathlib
 
 # Game constants
 # Window dimensions
+SCREEN_TITLE = "Rescue the Puppies"
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Rescue the Puppies"
 
-# Assets path
-ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent / "assets"
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 2
+TILE_SCALING = 2.0
+COIN_SCALING = 2.0
 
-class Platformer(arcade.Window):
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+class RescueThePuppies(arcade.Window):
+    """
+    Main application class.
+    """
+
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, resizable=False)
 
-        # These lists will hold different sets of sprites
-        self.coins = None
-        self.background = None
-        self.walls = None
-        self.ladders = None
-        self.goals = None
-        self.enemies = None
+        # Our Scene Object
+        self.scene = None
 
-        # One sprite for the player, no more is needed
-        self.player = None
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
 
-        # We need a physics engine as well
+        # Our physics engine
         self.physics_engine = None
 
-        # Someplace to keep score
-        self.score = 0
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
 
-        # Which level are we on?
-        self.level = 1
+        # A Camera that can be used to draw GUI elements
+        self.gui_camera = None
+
+        # Keep track of the score and lifes
+        self.score = 0
+        self.lifes = 3
+
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound("assets/sounds/handleCoins.wav")
+        self.jump_sound = arcade.load_sound("assets/sounds/confirmation_001.wav")
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
-        """Sets up the game for the current level"""
-        pass
+        """Set up the game here. Call this function to restart the game."""
+
+        # Set up the Camera
+        self.camera = arcade.Camera(self.width, self.height)
+
+        # Set up the GUI Camera
+        self.gui_camera = arcade.Camera(self.width, self.height)
+
+        # Keep track of the score
+        self.score = 0
+
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "assets/platformerart_pixelredux_2/Tiles/tile_0019.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite("assets/platformerart_pixelredux_2/Tiles/tile_0121.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                "assets/platformerart_pixelredux_2/Tiles/tile_0131.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
+
+        # Use a loop to place some coins for our character to pick up
+        for x in range(128, 1250, 256):
+            coin = arcade.Sprite("assets/platformerart_pixelredux_2/Tiles/tile_0078.png", COIN_SCALING)
+            coin.center_x = x
+            coin.center_y = 96
+            self.scene.add_sprite("Coins", coin)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
+        )
 
     def on_key_press(self, key: int, modifiers: int):
-        """Processes key presses
+        """Called whenever a key is pressed."""
 
-        Arguments:
-            key {int} -- Which key was pressed
-            modifiers {int} -- Which modifiers were down at the time
-        """
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                arcade.play_sound(self.jump_sound)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key: int, modifiers: int):
-        """Processes key releases
+        """Called when the user releases a key."""
 
-        Arguments:
-            key {int} -- Which key was released
-            modifiers {int} -- Which modifiers were down at the time
-        """
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
 
     def on_update(self, delta_time: float):
-        """Updates the position of all game objects
+        """Movement and game logic"""
 
-        Arguments:
-            delta_time {float} -- How much time since the last call
-        """
-        pass
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+        # See if we hit any coins
+        coin_hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.scene["Coins"]
+        )
+
+        # Loop through each coin we hit (if any) and remove it
+        for coin in coin_hit_list:
+            # Remove the coin
+            coin.remove_from_sprite_lists()
+            # Play a sound
+            arcade.play_sound(self.collect_coin_sound)
+            # Add one to the score
+            self.score += 1
+
+        # Position the camera
+        self.center_camera_to_player()
 
     def on_draw(self):
-        pass
+        """Render the screen."""
 
-if __name__ == "__main__":
-    window = Platformer()
+        # Clear the screen to the background color
+        self.clear()
+
+        # Activate our Camera
+        self.camera.use()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate the GUI camera before drawing GUI elements
+        self.gui_camera.use()
+
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"Score: {self.score}    Lifes: {self.lifes}"
+        arcade.draw_text(
+            score_text,
+            10,
+            SCREEN_HEIGHT-30,
+            arcade.csscolor.WHITE,
+            18,
+        )
+
+def main():
+    """Main function"""
+    window = RescueThePuppies()
     window.setup()
     arcade.run()
+
+if __name__ == "__main__":
+    main()
